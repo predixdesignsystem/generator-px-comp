e78b88f5764465f49dfb65fb73ae961b558d049a
Martin Wragg <mdwragg@gmail.com>
2016-08-16 15:42:49 -0700

    Fixing up yeoman for gulp.

--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -7,7 +7,9 @@ const gulpSequence = require('gulp-sequence');
 const importOnce = require('node-sass-import-once');
 const stylemod = require('gulp-style-modules');
 const browserSync = require('browser-sync').create();
-
+const gulpif = require('gulp-if');
+const combiner = require('stream-combiner2');
+const bump = require('gulp-bump');
 
 const sassOptions = {
   importer: importOnce,
@@ -23,46 +25,46 @@ gulp.task('clean', function() {
   }).pipe($.clean());
 });
 
-gulp.task('sass', function() {
-  return gulp.src(`./sass/${pkg.name}-predix.scss`)
-    .pipe($.sass(sassOptions).on('error', $.sass.logError))
-    .pipe($.autoprefixer({
+function buildCSS(){
+  return combiner.obj([
+    $.sass(sassOptions).on('error', $.sass.logError),
+    $.autoprefixer({
       browsers: ['last 2 versions', 'Safari 8.0'],
       cascade: false
-    }))
-    .pipe($.rename(`${pkg.name}.css`))
-    .pipe(gulp.dest('css'))
+    }),
+    $.cssmin()
+  ]);
+}
+
+gulp.task('sass', function() {
+  return gulp.src(['./sass/*.scss', '!./sass/*sketch.scss', '!./sass/*-demo.scss'])
+    .pipe(buildCSS())
+    .pipe(gulpif(/.*predix/,
+      $.rename(function(path){
+        path.basename = new RegExp('.+?(?=\-predix)').exec(path.basename)[0];
+      })
+    ))
     .pipe(stylemod({
-      // All files will be named 'styles.html'
-      filename: 'styles',
-      // Use '-css' suffix instead of '-styles' for module ids
       moduleId: function(file) {
-        return path.basename(file.path, path.extname(file.path)) + '-css';
+        return path.basename(file.path, path.extname(file.path)) + '-styles';
       }
     }))
-    .pipe($.rename(`${pkg.name}-styles.html`))
-    .pipe(gulp.dest('.'))
-    // .pipe(browserSync.stream());
-    .pipe(browserSync.stream({match: '**/*.css'}));
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.stream({match: 'css/*.html'}));
 });
 
-gulp.task('css', function() {
-  return gulp.src('css/**/*.css')
-    .pipe($.sourcemaps.init())
-    .pipe($.cssmin())
-    .pipe($.concat(pkg.name + '.css'))
-    .pipe($.sourcemaps.write('.'))
-    .pipe($.rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('css'));
+gulp.task('demosass', function() {
+  return gulp.src(['./sass/*-demo.scss'])
+    .pipe(buildCSS())
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.stream({match: '**/*.css'}));
 });
 
 gulp.task('watch', function() {
   gulp.watch('./sass/**/*.scss', ['default']);
 });
 
-gulp.task('serve', ['default'], function() {
+gulp.task('serve', function() {
   browserSync.init({
     port: 8080,
     notify: false,
@@ -74,16 +76,26 @@ gulp.task('serve', ['default'], function() {
   });
 
   gulp.watch(['!${pkg.name}-styles.html', '*.html', 'bower_components/**/*.html']).on('change', browserSync.reload);
-  gulp.watch('sass/*.scss', ['default']);
+  gulp.watch('sass/*.scss', ['sass']);
 
 });
 
-// gulp.task('bump', function() {
-//
-// });
-//
-// gulp.task('hint', function() {
-//
-// });
+gulp.task('bump:patch', function(){
+  gulp.src(['./bower.json', './package.json'])
+  .pipe(bump({type:'patch'}))
+  .pipe(gulp.dest('./'));
+});
 
-gulp.task('default', gulpSequence('clean', 'sass', 'css'));
+gulp.task('bump:minor', function(){
+  gulp.src(['./bower.json', './package.json'])
+  .pipe(bump({type:'minor'}))
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump:major', function(){
+  gulp.src(['./bower.json', './package.json'])
+  .pipe(bump({type:'major'}))
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', gulpSequence('clean', 'sass', 'demosass'));
